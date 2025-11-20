33f9cf7b1f530ac484fd88b1847db020466b381d
smarthomesven <sven@dypodex.com>
2025-11-20 18:45:22 +0100

    Added settings page

---
+++ api.js
@@ -0,0 +1,88 @@
+const axios = require('axios');
+
+module.exports = {
+  async getLinksData({ homey }) {
+    try {
+      const accessToken = homey.settings.get('accessToken');
+      if (!accessToken) {
+        throw new Error('Not logged in');
+      }
+
+      // Get all tenants (homes) for the user
+      const tenantsResponse = await axios.get('https://v5.api.cloudgarden.nl/tenant/?tenantQueryType=1&issuesOnly=false&sortDescendent=false&skip=0&take=25&returnModel=2', {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      });
+
+      const tenants = tenantsResponse.data.data;
+      
+      // Filter out the parent Duux tenant (id 44)
+      const userTenants = tenants.filter(tenant => tenant.parentTenantId !== null);
+      
+      // Fetch devices from all user tenants
+      const allDevicesWithStatus = [];
+      for (const tenant of userTenants) {
+        try {
+          const devicesResponse = await axios.get(`https://v5.api.cloudgarden.nl/sensor/?tenantId=${tenant.id}&returnModel=2&skip=0&take=25`, {
+            headers: {
+              'Authorization': `Bearer ${accessToken}`
+            }
+          });
+          
+          const devices = devicesResponse.data.data;
+          
+          // Fetch status for each device
+          for (const device of devices) {
+            try {
+              const statusResponse = await axios.get(
+                `https://v5.api.cloudgarden.nl/data/${device.deviceId}/status`,
+                {
+                  headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                  }
+                }
+              );
+              
+              allDevicesWithStatus.push({
+                tenantName: tenant.name,
+                tenantId: tenant.id,
+                deviceInfo: {
+                  id: device.id,
+                  displayName: device.displayName,
+                  deviceId: device.deviceId,
+                  type: device.type,
+                  spaceId: device.spaceId,
+                  name: device.name
+                },
+                status: statusResponse.data.data
+              });
+            } catch (statusError) {
+              // If status fetch fails, still include the device info
+              allDevicesWithStatus.push({
+                tenantName: tenant.name,
+                tenantId: tenant.id,
+                deviceInfo: {
+                  id: device.id,
+                  displayName: device.displayName,
+                  deviceId: device.deviceId,
+                  type: device.type,
+                  spaceId: device.spaceId,
+                  name: device.name
+                },
+                statusError: statusError.message
+              });
+            }
+          }
+        } catch (error) {
+          homey.app.error(`Error fetching devices for tenant ${tenant.id}:`, error.message);
+        }
+      }
+
+      return allDevicesWithStatus;
+    } catch (error) {
+      homey.app.error('Error in getLinksData:', error.message);
+      return { error: error.message };
+    }
+  }
+};
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Homey = require('homey');
+const axios = require('axios');
 
 module.exports = class DuuxV2App extends Homey.App {
 
@@ -9,6 +10,58 @@ module.exports = class DuuxV2App extends Homey.App {
    */
   async onInit() {
     this.log('Duux Gen2 has been initialized');
+  }
+
+  async getHomeData() {
+    try {
+            const accessToken = this.homey.settings.get('accessToken');
+            if (!accessToken) {
+              throw new Error('Not logged in');
+            }
+      
+            // Get all tenants (homes) for the user
+            const tenantsResponse = await axios.get('https://v5.api.cloudgarden.nl/tenant/?tenantQueryType=1&issuesOnly=false&sortDescendent=false&skip=0&take=25&returnModel=2', {
+              headers: {
+                'Authorization': `Bearer ${accessToken}`
+              }
+            });
+      
+            const tenants = tenantsResponse.data.data;
+            
+            // Filter out the parent Duux tenant (id 44)
+            const userTenants = tenants.filter(tenant => tenant.parentTenantId !== null);
+            
+            // Fetch devices from all user tenants
+            const allDevices = [];
+            for (const tenant of userTenants) {
+              try {
+                const devicesResponse = await axios.get(`https://v5.api.cloudgarden.nl/sensor/?tenantId=${tenant.id}&returnModel=2&skip=0&take=25`, {
+                  headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                  }
+                });
+                
+                // Add tenant info to each device for reference
+                const devicesWithTenant = devicesResponse.data.data.map(device => ({
+                  ...device,
+                  tenantId: tenant.id,
+                  tenantName: tenant.name
+                }));
+                
+                allDevices.push(...devicesWithTenant);
+              } catch (error) {
+                this.error(`Error fetching devices for tenant ${tenant.id}:`, error.message);
+              }
+            }
+      
+            // Filter for type 56 (as STRING!)
+            return allDevices;
+      
+      return availableDevices;
+    } catch (error) {
+      this.error('Error in getAllLinksData:', error.message);
+      return { error: error.message };
+    }
   }
 
 };
