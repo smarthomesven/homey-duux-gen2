5d512701019168d74eab9415431cba7b21ec7b78
smarthomesven <sven@dypodex.com>
2026-03-29 05:45:11 +0200

    Added Whisper Flex 2

---
+++ drivers/whisper-flex-2/device.js
@@ -0,0 +1,414 @@
+'use strict';
+
+const Homey = require('homey');
+const axios = require('axios');
+const https = require('https');
+const CacheableLookup = require('cacheable-lookup');
+const cacheable = new CacheableLookup({
+  maxTtl: 300,
+});
+const httpsAgent = new https.Agent({
+  keepAlive: true,
+  keepAliveMsecs: 10000,
+  maxSockets: 10,
+  maxFreeSockets: 4,
+  timeout: 30000,
+});
+cacheable.install(httpsAgent);
+const apiClient = axios.create({
+  baseURL: 'https://v5.api.cloudgarden.nl',
+  httpsAgent,
+  timeout: 10000,
+});
+
+module.exports = class WhisperFlexDevice extends Homey.Device {
+
+  /**
+   * onInit is called when the device is initialized.
+   */
+  async onInit() {
+    try {
+      this.log('Whisper Flex device has been initialized');
+      
+      this.registerCapabilityListener("onoff", async (value) => {
+        let command;
+        if (value === true) {
+          command = "tune set power 1";
+        } else {
+          command = "tune set power 0";
+        }
+        await this.sendCommand(command);
+      });
+
+      this.registerCapabilityListener("fan_speed", async (value) => {
+        let command;
+        command = `tune set speed ${value}`;
+        await this.sendCommand(command);
+      });
+
+      this.registerCapabilityListener("whisper_mode_2", async (value) => {
+        let command;
+        if (value === "normal") {
+          command = "tune set mode 0";
+        } else if (value === "nature") {
+          command = "tune set mode 1";
+        } else return;
+        await this.sendCommand(command);
+      });
+
+      this.registerCapabilityListener("horizontal_oscillation2", async (value) => {
+        let command;
+        if (value === "30") {
+          command = "tune set horosc 1";
+        } else if (value === "45") {
+          command = "tune set horosc 2";
+        } else if (value === "90") {
+          command = "tune set horosc 3";
+        } else if (value === "off") {
+          command = "tune set horosc 0"
+        } else return;
+        await this.sendCommand(command);
+      });
+
+      this.registerCapabilityListener("vertical_oscillation2", async (value) => {
+        let command;
+        if (value === "45") {
+          command = "tune set horosc 1";
+        } else if (value === "100") {
+          command = "tune set horosc 2";
+        } else if (value === "off") {
+          command = "tune set horosc 0"
+        } else return;
+        await this.sendCommand(command);
+      });
+
+      this.registerCapabilityListener("child_lock", async (value) => {
+        let command;
+        if (value === true) {
+          command = "tune set lock 1";
+        } else {
+          command = "tune set lock 0";
+        }
+        await this.sendCommand(command);
+      });
+
+      this.registerCapabilityListener("night_mode", async (value) => {
+        let command;
+        if (value === true) {
+          command = "tune set night 1";
+        } else {
+          command = "tune set night 0";
+        }
+        await this.sendCommand(command);
+      });
+
+      this.setStoreValue('firstRun', true);
+
+      this.startPolling();
+
+      const setHorizontalOscillationAction = this.homey.flow.getActionCard('set_horizontal_oscillation');
+      const setVerticalOscillationAction = this.homey.flow.getActionCard('set_vertical_oscillation');
+      const setModeAction = this.homey.flow.getActionCard('set_whisper_flex_2_mode');
+      const enableChildLockAction = this.homey.flow.getActionCard('enable_child_lock_whisper');
+      const disableChildLockAction = this.homey.flow.getActionCard('disable_child_lock_whisper');
+      const enableNightModeAction = this.homey.flow.getActionCard('enable_night_mode_whisper');
+      const disableNightModeAction = this.homey.flow.getActionCard('disable_night_mode_whisper');
+      const childLockCondition = this.homey.flow.getConditionCard('whisper_child_lock_condition');
+      const nightModeCondition = this.homey.flow.getConditionCard('whisper_night_mode_condition');
+
+
+      setModeAction.registerRunListener(async (args, state) => {
+        if (args.mode === 'normal') {
+          await this.sendCommand("tune set mode 0");
+        } else if (args.mode === 'nature') {
+          await this.sendCommand("tune set mode 1");
+        }
+        return true;
+      });
+
+      setHorizontalOscillationAction.registerRunListener(async (args, state) => {
+        if (args.mode === 'off') {
+          await this.sendCommand("tune set horosc 0");
+        } else if (args.mode === '30') {
+          await this.sendCommand("tune set horosc 1");
+        } else if (args.mode === '60') {
+          await this.sendCommand("tune set horosc 2");
+        } else if (args.mode === '90') {
+          await this.sendCommand("tune set horosc 3");
+        }
+        return true;
+      });
+
+      setVerticalOscillationAction.registerRunListener(async (args, state) => {
+        if (args.mode === 'off') {
+          await this.sendCommand("tune set verosc 0");
+        } else if (args.mode === '45') {
+          await this.sendCommand("tune set verosc 1");
+        } else if (args.mode === '100') {
+          await this.sendCommand("tune set verosc 2");
+        }
+        return true;
+      });
+
+      enableChildLockAction.registerRunListener(async (args, state) => {
+        await this.sendCommand("tune set lock 1");
+        return true;
+      });
+
+      disableChildLockAction.registerRunListener(async (args, state) => {
+        await this.sendCommand("tune set lock 0");
+        return true;
+      });
+
+      enableNightModeAction.registerRunListener(async (args, state) => {
+        await this.sendCommand("tune set night 1");
+        return true;
+      });
+
+      disableNightModeAction.registerRunListener(async (args, state) => {
+        await this.sendCommand("tune set night 0");
+        return true;
+      });
+
+      childLockCondition.registerRunListener(async (args, state) => {
+        const isChildLock = this.getCapabilityValue('child_lock');
+        return isChildLock;
+      });
+
+      nightModeCondition.registerRunListener(async (args, state) => {
+        const isNightMode = this.getCapabilityValue('night_mode');
+        return isNightMode;
+      });
+    } catch (error) {
+      this.error('Error during Whisper Flex 2 device initialization:', error);
+    }
+  }
+
+  /**
+   * onAdded is called when the user adds the device, called just after pairing.
+   */
+  async onAdded() {
+    this.log('Whisper Flex 2 device has been added');
+  }
+
+  /**
+   * onSettings is called when the user updates the device's settings.
+   * @param {object} event the onSettings event data
+   * @param {object} event.oldSettings The old settings object
+   * @param {object} event.newSettings The new settings object
+   * @param {string[]} event.changedKeys An array of keys changed since the previous version
+   * @returns {Promise<string|void>} return a custom message that will be displayed
+   */
+  async onSettings({ oldSettings, newSettings, changedKeys }) {
+    this.log('Whisper Flex 2 device settings where changed');
+  }
+
+  /**
+   * onRenamed is called when the user updates the device's name.
+   * This method can be used this to synchronise the name to the device.
+   * @param {string} name The new name
+   */
+  async onRenamed(name) {
+    this.log('Whisper Flex 2 device was renamed');
+  }
+
+  /**
+   * onDeleted is called when the user deleted the device.
+   */
+  async onDeleted() {
+    this.log('Whisper Flex 2 device has been deleted');
+    this.stopPolling();
+  }
+
+  startPolling() {
+    this.stopPolling();
+    this.pollDeviceStatus();
+    this.pollInterval = this.homey.setInterval(() => {
+      this.pollDeviceStatus();
+    }, 15000);
+  }
+
+  stopPolling() {
+    if (this.pollInterval) {
+      this.homey.clearInterval(this.pollInterval);
+      this.pollInterval = null;
+    }
+  }
+
+  async pollDeviceStatus() {
+    try {
+      const accessToken = this.homey.settings.get('accessToken');
+      const mac = this.getStoreValue('mac');
+      
+      if (!accessToken || !mac) {
+        this.error('Missing accessToken or MAC address');
+        return;
+      }
+
+      const response = await apiClient.get(
+        `/data/${mac}/status`,
+        {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        }
+      );
+
+      const status = response.data.data;
+      
+      if (status) {
+        // Update onoff capability
+        const isOn = status.power === 1;
+        await this.setAvailable();
+        if (this.hasCapability('onoff')) {
+          await this.setCapabilityValue('onoff', isOn).catch(err => {
+            this.error('Error setting onoff capability:', err);
+          });
+        }
+
+        // Update fan speed capability
+        if (this.hasCapability('fan_speed') && status.sp !== undefined) {
+          const speed = status.speed;
+          await this.setCapabilityValue('fan_speed', speed).catch(err => {
+            this.error('Error setting fan speed capability:', err);
+          });
+        }
+
+        // Update battery capability
+        if (this.hasCapability('measure_battery') && status.batlvl !== undefined) {
+          const battery = status.batlvl;
+          if (battery === 0) {
+            await this.removeCapability('measure_battery');
+          }
+          await this.setCapabilityValue('measure_battery', battery).catch(err => {
+            this.error('Error setting measure_battery capability:', err);
+          });
+        }
+
+        // Log if device is in error state
+        if (status.err && status.err !== 0) {
+          this.error('Device reported error code:', status.err);
+        }
+
+        if (this.hasCapability('whisper_mode_2') && status.mode !== undefined) {
+          let modeValue;
+          if (status.mode === 0) {
+            modeValue = "normal";
+          } else if (status.mode === 1) {
+            modeValue = "nature";
+          } else return;
+          await this.setCapabilityValue('whisper_mode_2', modeValue).catch(err => {
+            this.error('Error setting mode capability:', err);
+          });
+        }
+
+        if (this.hasCapability('horizontal_oscillation2') && status.horosc !== undefined) {
+          let horizontalOscillationValue;
+          if (status.horosc === 0) {
+            horizontalOscillationValue = "off";
+          } else if (status.horosc === 1) {
+            horizontalOscillationValue = "30";
+          } else if (status.horosc === 2) {
+            horizontalOscillationValue = "60";
+          } else if (status.horosc === 3) {
+            horizontalOscillationValue = "90";
+          } else return;
+          await this.setCapabilityValue('horizontal_oscillation2', horizontalOscillationValue).catch(err => {
+            this.error('Error setting horizontal_oscillation2 capability:', err);
+          });
+        }
+
+        if (this.hasCapability('vertical_oscillation2') && status.verosc !== undefined) {
+          let verticalOscillationValue;
+          if (status.verosc === 0) {
+            verticalOscillationValue = "off";
+          } else if (status.verosc === 1) {
+            verticalOscillationValue = "45";
+          } else if (status.verosc === 2) {
+            verticalOscillationValue = "100";
+          } else return;
+          await this.setCapabilityValue('vertical_oscillation2', verticalOscillationValue).catch(err => {
+            this.error('Error setting vertical_oscillation2 capability:', err);
+          });
+        }
+
+        const isFirstRun = this.getStoreValue('firstRun');
+
+        const isChildLock = status.lock === 1;
+        this.setStoreValue('child_lock', isChildLock);
+        if (this.hasCapability('child_lock')) {
+          if (isChildLock !== this.getCapabilityValue('child_lock')) {
+            if (isFirstRun !== true) {
+              if (this.getCapabilityValue('child_lock') === true) {
+                await this.driver.triggerFlow('child_lock_enabled_whisper', this);
+              } else if (this.getCapabilityValue('child_lock') === false) {
+                await this.driver.triggerFlow('child_lock_enabled_whisper', this);
+              }
+            }
+          }
+          await this.setCapabilityValue('child_lock', isChildLock).catch(err => {
+            this.error('Error setting child_lock capability:', err);
+          });
+        }
+
+        const isNightMode = status.night === 1;
+        this.setStoreValue('night_mode', isNightMode);
+        if (this.hasCapability('night_mode')) {
+          if (isChildLock !== this.getCapabilityValue('night_mode')) {
+            if (isFirstRun !== true) {
+              if (this.getCapabilityValue('night_mode') === true) {
+                await this.driver.triggerFlow('night_mode_enabled_whisper', this);
+              } else if (this.getCapabilityValue('night_mode') === false) {
+                await this.driver.triggerFlow('night_mode_enabled_whisper', this);
+              }
+            }
+          }
+          await this.setCapabilityValue('night_mode', isNightMode).catch(err => {
+            this.error('Error setting night_mode capability:', err);
+          });
+        }
+
+        if (isFirstRun === true) {
+          this.setStoreValue('firstRun', false);
+        }
+      }
+    } catch (error) {
+      this.error('Error polling device status:', error.message);
+      
+      if (error.response && error.response.status === 401) {
+        await this.setUnavailable('Authentication required. Please re-login.').catch(err => {
+          this.error('Error setting unavailable:', err);
+        });
+      }
+    }
+  }
+
+  async sendCommand(command) {
+    try {
+      const accessToken = this.homey.settings.get('accessToken');
+      const mac = this.getStoreValue('mac');
+      
+      if (!accessToken || !mac) {
+        this.error('Missing accessToken or MAC address');
+      }
+
+      await apiClient.post(
+        `/sensor/${mac}/commands`,
+        {
+          command: command
+        },
+        {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+      
+      return true;
+    } catch (error) {
+      this.error('Error controlling device:', error.message);
+    }
+  }
+
+};
---
+++ drivers/whisper-flex-2/driver.js
@@ -0,0 +1,223 @@
+'use strict';
+
+const Homey = require('homey');
+const axios = require('axios');
+const crypto = require('crypto');
+
+module.exports = class WhisperFlex2Driver extends Homey.Driver {
+
+  /**
+   * onInit is called when the driver is initialized.
+   */
+  async onInit() {
+    this.log('Whisper Flex 2 driver has been initialized');
+  }
+
+  async triggerFlow(card_id, device) {
+    this.homey.flow.getDeviceTriggerCard(card_id).trigger(device, {}, {});
+  }
+
+  generateCodeVerifier() {
+    const buffer = crypto.randomBytes(64);
+    return this.base64URLEncode(buffer);
+  }
+
+  generateCodeChallenge(verifier) {
+    const hash = crypto.createHash('sha256').update(verifier).digest();
+    return this.base64URLEncode(hash);
+  }
+
+  base64URLEncode(buffer) {
+    return buffer.toString('base64')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=/g, '');
+  }
+
+  async onPair(session) {
+    this.log('Whisper Flex 2 pairing started');
+    this._type = "pair";
+    this._session = session;
+
+    session.setHandler('showView', async (viewId) => {
+      if (viewId === 'email') {
+        const loggedIn = this.homey.settings.get('loggedIn');
+        if (loggedIn) {
+          await session.showView('list_devices');
+          return;
+        }
+      }
+    });
+    
+    session.setHandler('login', async (data) => {
+      try {
+        this.homey.settings.set('email', data.email);
+        const codeVerifier = this.generateCodeVerifier();
+        const codeChallenge = this.generateCodeChallenge(codeVerifier);
+        this.homey.settings.set('verifier', codeVerifier);
+        this.homey.settings.set('challenge', codeChallenge);
+        this.log('Login with email:', data.email);
+
+        const redirectUri = await this.registerWebhook(session);
+        
+        const response = await axios.post('https://v5.api.cloudgarden.nl/auth/passwordlessLogin/code', {
+          email: data.email,
+          clientId: '83f34a5fa5faca9023c78980a57a87b41f6972fc4ee45e9c',
+          codeChallenge: codeChallenge,
+          codeChallengeMethod: 'sha256',
+          redirectUri: redirectUri,
+          tenantId: 44
+        });
+        
+        if (response.data && response.data.data === 'ok') {
+          await session.showView('code');
+        } else {
+          return false;
+        }
+      } catch (error) {
+        this.error('Login error:', error);
+        return false;
+      }
+    });
+
+    session.setHandler('code', async (data) => {
+      return await this.homey.app.codeLogin(data, session, "pair");
+    });
+
+    session.setHandler("list_devices", async () => {
+      try {
+        return await this.onPairListDevices();
+      } catch (error) {
+        throw new Error("Error while fetching devices: " + error.message);
+      }
+    });
+  }
+
+  async registerWebhook(session) {
+    try {
+      const cloudId = await this.homey.cloud.getHomeyId();
+      const id = Homey.env.WEBHOOK_ID;
+      const secret = Homey.env.WEBHOOK_SECRET;
+      const authWebhook = await this.homey.cloud.createWebhook(id, secret, {});
+      authWebhook.on('message', async args => {
+        try {
+        this.log('Got a webhook message!');
+        this.log('headers:', args.headers);
+        this.log('query:', args.query);
+        this.log('body:', args.body);
+        await this.homey.app.codeLogin({ code: args.query.code }, this._session, this._type);
+        } catch (error) {
+          this.error('Error handling webhook message:', error);
+        }
+      });
+      return `https://smarthomesven.github.io/homey-duux-gen2-auth/#/${cloudId}`;
+    } catch (error) {
+      this.error('Error registering webhook:', error);
+    }
+  }
+
+  async onRepair(session) {
+    this.log('Whisper Flex 2 repairing started');
+    this._type = "repair";
+    this._session = session;
+    
+    session.setHandler('login', async (data) => {
+      try {
+        this.homey.settings.set('email', data.email);
+        const codeVerifier = this.generateCodeVerifier();
+        const codeChallenge = this.generateCodeChallenge(codeVerifier);
+        this.homey.settings.set('verifier', codeVerifier);
+        this.homey.settings.set('challenge', codeChallenge);
+        this.log('Login with email:', data.email);
+
+        const redirectUri = await this.registerWebhook(this._session);
+        
+        const response = await axios.post('https://v5.api.cloudgarden.nl/auth/passwordlessLogin/code', {
+          email: data.email,
+          clientId: '83f34a5fa5faca9023c78980a57a87b41f6972fc4ee45e9c',
+          codeChallenge: codeChallenge,
+          codeChallengeMethod: 'sha256',
+          redirectUri: redirectUri,
+          tenantId: 44
+        });
+        
+        if (response.data && response.data.data === 'ok') {
+          await session.showView('code');
+        } else {
+          return false;
+        }
+      } catch (error) {
+        this.error('Login error:', error);
+        return false;
+      }
+    });
+
+    session.setHandler('code', async (data) => {
+      return await this.homey.app.codeLogin(data, session, "repair");
+    });
+  }
+
+  /**
+   * onPairListDevices is called when a user is adding a device
+   * and the 'list_devices' view is called.
+   * This should return an array with the data of devices that are available for pairing.
+   */
+  async onPairListDevices() {
+    try {
+      const accessToken = this.homey.settings.get('accessToken');
+      if (!accessToken) {
+        throw new Error('Not logged in');
+      }
+
+      const tenantsResponse = await axios.get('https://v5.api.cloudgarden.nl/tenant/?tenantQueryType=1&issuesOnly=false&sortDescendent=false&skip=0&take=25&returnModel=2', {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      });
+
+      const tenants = tenantsResponse.data.data;    
+      const userTenants = tenants.filter(tenant => tenant.parentTenantId !== null); 
+      const allDevices = [];
+      for (const tenant of userTenants) {
+        try {
+          const devicesResponse = await axios.get(`https://v5.api.cloudgarden.nl/sensor/?tenantId=${tenant.id}&returnModel=2&skip=0&take=25`, {
+            headers: {
+              'Authorization': `Bearer ${accessToken}`
+            }
+          });
+          
+          const devicesWithTenant = devicesResponse.data.data.map(device => ({
+            ...device,
+            tenantId: tenant.id,
+            tenantName: tenant.name
+          }));
+          
+          allDevices.push(...devicesWithTenant);
+        } catch (error) {
+          this.error(`Error fetching devices for tenant ${tenant.id}:`, error.message);
+        }
+      }
+
+      const availableDevices = allDevices
+        .filter(device => device.type === "58")
+        .map(device => ({
+          name: device.displayName,
+          data: {
+            id: device.id
+          },
+          store: {
+            id: device.id,
+            mac: device.deviceId,
+            tenantId: device.tenantId,
+            spaceId: device.spaceId,
+            type: device.type
+          }
+        }));
+
+      return availableDevices;
+    } catch (error) {
+      this.error('Error in onPairListDevices:', error.message);
+    }
+  }
+
+};
